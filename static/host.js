10628725ce64803bba77211db20ddb1835f2abcd
qompute <qhchen09@gmail.com>
2021-06-05 18:55:57 -0700

    changed host page to show requested song

--- static/host.js
+++ static/host.js
@@ -1,5 +1,6 @@
 const source = new EventSource('/stream');
 source.addEventListener('Song request', function(event) {
+  let song = JSON.parse(event.data);
   let requestLabel = document.getElementById('request_label');
-  requestLabel.innerHTML = event.data;
+  requestLabel.innerHTML = song.name + ' - ' + song.artists.join(', ');
 });
